3651fe17f6efe89fc37c165c4070e60cc452e3c1
Snap <XxSnapperGeexX@gmail.com>
2023-10-18 20:54:54 -0700

    Add message to returned json in delete notes.

--- controllers/delete-notes.mjs
+++ controllers/delete-notes.mjs
@@ -10,17 +10,23 @@ export const deleteNotes = (req, res) =>
 
     if (indexOfNoteWithId === -1)
     {
-        res.json({deleted: null, error: null});
+        res.json({message: `No id: ${noteId}`, deleted: null, error: null});
         return;
     }
 
-    const resObj = { deleted: notes.splice(indexOfNoteWithId, 1)[0], error: null }
+    const resObj = {deleted: notes.splice(indexOfNoteWithId, 1)[0], error: null}
 
     const pathToDb = resolvePath("db", "db.json");
 
     writeFile(pathToDb, JSON.stringify(notes, null, 2), (err) =>
     {
-        if (err) { resObj.error = err; };
+        if (err)
+        {
+            resObj.message = "Error writing to db.json";
+            resObj.error = err;
+        };
+
+        resObj.message = `Deleted note with id: ${noteId}`;
     });
 
     res.json(resObj);
