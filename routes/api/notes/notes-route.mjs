b75aaf4b9dd2a4b9eb1deea41ca5807b2336544a
Snap <XxSnapperGeexX@gmail.com>
2023-10-18 18:45:13 -0700

    Bind post and delete notes to crosponding route request.

--- routes/api/notes/notes-route.mjs
+++ routes/api/notes/notes-route.mjs
@@ -1,8 +1,12 @@
 import { getNotes } from "../../../controllers/get-notes.mjs";
+import { postNotes } from "../../../controllers/post-notes.mjs";
+import { deleteNotes } from "../../../controllers/delete-notes.mjs";
 import { Router } from "express";
 
 export const route = Router();
 
 route.get("/", getNotes);
+route.post("/", postNotes);
+route.delete("/", deleteNotes);
 
 export default route;
