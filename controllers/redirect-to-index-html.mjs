755833c15587132a1af0ce18ca3d0f87cf3d8837
Snap <XxSnapperGeexX@gmail.com>
2023-10-18 22:10:25 -0700

    Create fallback redirectToIndex function and set in server.mjs.

---
+++ controllers/redirect-to-index-html.mjs
@@ -0,0 +1,6 @@
+export const redirectToIndex = (req, res) =>
+{
+    res.redirect("/index.html");
+};
+
+export default redirectToIndex;
--- server.mjs
+++ server.mjs
@@ -1,3 +1,4 @@
+import redirectToIndex from "./controllers/redirect-to-index-html.mjs";
 import { route as routes } from "./routes/index.mjs";
 import express from "express";
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(express.static("public"));
+
+app.get("*", redirectToIndex);
 
 app.listen(PORT, () => console.log(`Express server listening on http://localhost:${PORT}...`));
