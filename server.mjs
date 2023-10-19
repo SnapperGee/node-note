519889e07d6d53fb0e5b01fc47d9407843c25362
Snap <XxSnapperGeexX@gmail.com>
2023-10-18 17:15:27 -0700

    Use routes.

--- server.mjs
+++ server.mjs
@@ -1,9 +1,11 @@
+import { route as routes } from "./routes/index.mjs";
 import express from "express";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(routes);
 app.use(express.static("public"));
 
 app.listen(PORT, () => console.log(`Express server listening on PORT ${PORT}...`));
